b46a24b0d44b29c8916959cc62cac4a53f3a7cdd
mlipson <mattl920@gmail.com>
2014-04-13 09:57:30 -0400

    ship paused with rest of scene

--- game_v1/src/components.js
+++ game_v1/src/components.js
@@ -164,16 +164,18 @@ Crafty.c('PlayerCharacter', {
         // Watch for a change of direction and switch animations accordingly
         var animation_speed = 4;
         this.bind('NewDirection', function (data) {
-            if (data.x > 0) {
-                this.animate('PlayerMovingRight', animation_speed, -1);
-            } else if (data.x < 0) {
-                this.animate('PlayerMovingLeft', animation_speed, -1);
-            } else if (data.y > 0) {
-                this.animate('PlayerMovingDown', animation_speed, -1);
-            } else if (data.y < 0) {
-                this.animate('PlayerMovingUp', animation_speed, -1);
-            } else {
-                this.stop();
+            if (!Game.paused) {
+                if (data.x > 0) {
+                    this.animate('PlayerMovingRight', animation_speed, -1);
+                } else if (data.x < 0) {
+                    this.animate('PlayerMovingLeft', animation_speed, -1);
+                } else if (data.y > 0) {
+                    this.animate('PlayerMovingDown', animation_speed, -1);
+                } else if (data.y < 0) {
+                    this.animate('PlayerMovingUp', animation_speed, -1);
+                } else {
+                    this.stop();
+                }
             }
         });
         this.bind('Moved', function () {
