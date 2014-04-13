ac86b9c1e963ac27f1b19cf779fbc36b52e4b82d
jeff <jeff@jeff-port-linux.(none)>
2014-04-13 13:11:51 -0400

    Final fix to info box

--- game_v1/src/components.js
+++ game_v1/src/components.js
@@ -1,5 +1,237 @@
 // The Grid component allows an element to be located
 //  on a grid of tiles
+
+
+var SPECTRAL_INDEX = {
+  '?': {},
+  'A': {},
+  'B': {
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+    'iron': 10,
+  },
+  'C': {
+//    # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .2,
+    'iron': .166,
+    'nickel': .014,
+    'cobalt': .002,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+
+ //   # Original estimates:
+
+//    #'water': 1.55495461,
+   // #'platinum': 0.01,
+
+ //   # lewis estimates
+ //   #'iron': 62.196,
+ //   #'nickel': 1.26,
+  //  #'cobalt': 0.18,
+  },
+  'Ch': {
+ //   # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .2,
+    'iron': .166,
+    'nickel': .014,
+    'cobalt': .002,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'Cg': {
+ //   # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .2,
+    'iron': .166,
+    'nickel': .014,
+    'cobalt': .002,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'Cgh': {
+  //  # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .2,
+    'iron': .166,
+    'nickel': .014,
+    'cobalt': .002,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'C type': {
+   // # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .2,
+    'iron': .166,
+    'nickel': .014,
+    'cobalt': .002,
+
+  //  # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'Cb': { //  # transition object between C and B
+  //  #'hydrogen': 0.1175,
+  //  #'iron': 12.5,
+  //  #'water': 0.775,
+  //  #'nickel-iron': 0.0434,
+ //   #'platinum': 0.005,
+//
+//    # lewis estimates
+  //  #'iron': 36,
+  //  #'nickel': 0.63,
+  //  #'cobalt': 0.09,
+
+ //   # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .1,
+    'iron': .083,
+    'nickel': .007,
+    'cobalt': .001,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'D': {
+    'water': 0.000023,
+  },
+  'E': {
+
+  },
+  'K': { // # cross between S and C
+//    #'water': 0.775,
+//    #'nickel-iron': 0.0434,
+//    #'platinum': 0.005,
+
+//    # lewis estimates
+//    #'iron': 31.098,
+//    #'nickel': 0.13,
+//    #'cobalt': 0.09,
+
+//    # from Keck report at http://www.kiss.caltech.edu/study/asteroid/asteroid_final_report.pdf
+    'water': .1,
+    'iron': .083,
+    'nickel': .007,
+    'cobalt': .001,
+
+//    # volatiles
+    'hydrogen': 0.235,
+    'nitrogen': 0.001,
+    'ammonia': 0.001,
+  },
+  'L': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+    'aluminum': 7
+  },
+  'Ld': { // # copied from S
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'M': {
+    'iron': 88,
+    'nickel': 10,
+    'cobalt': 0.5,
+  },
+  'O': {
+    'nickel-iron': 2.965,
+    'platinum': 1.25,
+  },
+  'P': { // # correspond to CI, CM carbonaceous chondrites
+    'water': 12.5,
+  },
+  'R': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'S': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  //# Sa, Sq, Sr, Sk, and Sl all transition objects (assume half/half)
+  'Sa': {
+    'magnesium silicate': 5e-31,
+    'iron silicate': 0,
+  },
+  'Sq': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'Sr': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'Sk': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'Sl': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'S(IV)': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'Q': {
+    'nickel-iron': 13.315,
+  },
+  'R': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+  'T': {
+    'iron': 6,
+  },
+  'U': {
+
+  },
+  'V': {
+    'magnesium silicate': 1e-30,
+    'iron silicate': 0,
+  },
+
+  //# TODO use density to decide on what kind of X?
+  'X': { // # TODO these vals only apply to M-type within X
+    'iron': 88,
+    'nickel': 10,
+    'cobalt': 0.5,
+  },
+  'Xe': {  ////# TODO these vals only apply to M-type within X
+    'iron': 88,
+    'nickel': 10,
+    'cobalt': 0.5,
+  },
+  'Xc': {  
+    'iron': 88,
+    'nickel': 10,
+    'cobalt': 0.5,
+    'platinum': 0.005,
+  },
+  'Xk': { 
+    'iron': 88,
+    'nickel': 10,
+    'cobalt': 0.5,
+  },
+  'comet': {
+  },
+}
+
+
+
+
 Crafty.c('Grid', {
     init: function () {
         this.attr({
@@ -189,11 +421,46 @@ Crafty.c('Rock', {
             info_box.x = this._x + 15;
             info_box.y = this._y - 45;
 
-            if (this.isprobed) {
-                info_box.text('Asteroid ' + this.asteroid_data.prov_des + ': $' + this.asteroid_data.price);
-            } else {
-                info_box.text('Asteroid ' + this.asteroid_data.prov_des);
-            }
+
+ 	    unprobed = '<p>Unexplored!</p>';
+ 	    spec_type = 'Unknown';
+            tval = 'Unknown';
+            dist = this.asteroid_data.earth_dist.toFixed(2) + ' AU';
+ 	    deltav = this.asteroid_data.earth_dv.toFixed(2) + ' km/s';
+ 	    minerals = 'Unknown';
+ 	    price_per_kg = 'Unknown';
+ 	    pha = this.asteroid_data.pha=='Y' ? '<p class="hazard">Potentially Hazardous Object!</p>' : '';
+ 	    neo = this.asteroid_data.neo=='Y' ? '<p>Near Earth Object</p>' : '';  
+	    
+            if (this.isprobed)
+	    {
+ 		unprobed = '';
+ 		spec_type = this.asteroid_data.spec;
+ 		tval = '$' + this.asteroid_data.price.toLocaleString();
+ 		minerals_key = SPECTRAL_INDEX[this.asteroid_data.spec];
+		minerals = '';
+		for (key in minerals_key)
+		{
+			minerals = minerals + ' ' + key;
+		}
+ 		price_per_kg = '$' + this.asteroid_data.value.toLocaleString();
+ 	    }
+ 	    html = '<p>Asteroid ' + this.asteroid_data.full_name + '</p>';
+ 	    html = html + unprobed;
+ 	    html = html + '<p>Spectral Type: ' + spec_type + '</p>';
+            html = html + '<p>Minerals: ' + minerals + '</p>';
+            html = html + '<p>Price per kg: ' + price_per_kg + '</p>';
+ 	    html = html + '<p>Total Value: ' + tval + '</p>';
+            html = html + '<p>Distance: ' + dist + '</p>';
+            html = html + '<p>Delta-V: ' + deltav + '</p>';
+            html = html + neo + pha;
+
+	console.log(html);
+
+            info_box.text(html);
+
+
+
             this.attach(info_box);
             info_box.css({ display: 'block' });
 			testGlobal=info_box;
