0a9a69d904461a2331514d5af638d5163fab9d81
mlipson <mattl920@gmail.com>
2014-04-17 21:54:23 -0400

    refactored some of the components into their own file

--- game_v1/src/components.js
+++ game_v1/src/components.js
@@ -55,160 +55,12 @@ Crafty.c('GasStation', {
 });
 
 
-Crafty.c('BuyProbe', {
-    init: function () {
-        this.requires('Actor, spr_buy_probe, Mouse, HTML');
-
-        var info_box = Crafty.e("2D, DOM, Text, infoBox")
-            .attr({w: 75, alpha: 0.8})
-            .text('Buy Probe ($100000)')
-            .css({
-                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
-
-        this.bind('Click', function (data) {
-            activeShip.destroy();
-            activeShip = Crafty.e('Probe').at(38, 38);
-            updateScore(-100000);
-        });
-        this.bind('MouseOver', function (data) {
-            info_box.x = this._x + 15;
-            info_box.y = this._y - 45;
-            $('#' + info_box.getDomId()).css('height', 'inherit');
-            this.attach(info_box);
-            info_box.css({ display: 'block' });
-        });
-        this.bind('MouseOut', function (data) {
-            info_box.css({display: 'None'});
-        });
-    }
-});
-
-Crafty.c('BuyHRefinery', {
-    init: function () {
-        this.requires('Actor, spr_h_refinery, Mouse, HTML');
-
-        var info_box = Crafty.e("2D, DOM, Text, infoBox")
-            .attr({w: 75, alpha: 0.8})
-            .text('Buy Refinery ($1000000)')
-            .css({
-                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
-
-        this.bind('Click', function (data) {
-            Crafty.e('HRefinery').at(20, 36);
-            updateScore(-1000000);
-        });
-        this.bind('MouseOver', function (data) {
-            info_box.x = this._x + 15;
-            info_box.y = this._y - 45;
-            $('#' + info_box.getDomId()).css('height', 'inherit');
-            this.attach(info_box);
-            info_box.css({ display: 'block' });
-        });
-        this.bind('MouseOut', function (data) {
-            info_box.css({display: 'None'});
-        });
-    }
-});
-
-Crafty.c('BuyGasStation', {
-    init: function () {
-        this.requires('Actor, spr_gas_station, Mouse, HTML, infoBox');
-
-        var info_box = Crafty.e("2D, DOM, Text")
-            .attr({w: 75, alpha: 0.8})
-            .text('Buy Space Gas Station ($1000000)')
-            .css({
-                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
-
-        this.bind('Click', function (data) {
-            gasStation = Crafty.e('GasStation').at(20, 37);
-            gasStation.fueled = false;
-            updateScore(-1000000);
-        });
-        this.bind('MouseOver', function (data) {
-            info_box.x = this._x + 15;
-            info_box.y = this._y - 45;
-            $('#' + info_box.getDomId()).css('height', 'inherit');
-            this.attach(info_box);
-            info_box.css({ display: 'block' });
-        });
-        this.bind('MouseOut', function (data) {
-            info_box.css({display: 'None'});
-        });
-    }
-});
-
-Crafty.c('BuyShip', {
-    init: function () {
-        this.requires('Actor, spr_buy_ship, Mouse, HTML');
-
-        var info_box = Crafty.e("2D, DOM, Text")
-            .attr({w: 75, alpha: 0.8})
-            .text('Buy Ship ($300000)')
-            .css({
-                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
-
-        this.bind('Click', function (data) {
-            activeShip.destroy();
-            activeShip = Crafty.e('Ship').at(38, 38);
-            updateScore(-300000);
-        });
-        this.bind('MouseOver', function (data) {
-            info_box.x = this._x + 15;
-            info_box.y = this._y - 45;
-            $('#' + info_box.getDomId()).css('height', 'inherit');
-            this.attach(info_box);
-            testGlobal = info_box;
-            info_box.css({ display: 'block' });
-        });
-        this.bind('MouseOut', function (data) {
-            info_box.css({display: 'None'});
-        });
-    }
-});
-
 var testGlobal = undefined;
 
 // A Rock is just an Actor with a certain sprite
 Crafty.c('Rock', {
     init: function () {
-        this.requires('Actor, spr_rock, Mouse, Canvas')
+        this.requires('Actor, spr_rock, Mouse')
             .origin('center')
             .attr({ isprobed: false });
 
@@ -402,7 +254,7 @@ Crafty.c('ISS', {
 // This is the player-controlled character
 Crafty.c('Ship', {
     init: function () {
-        var classes = 'Actor, Canvas, Controls, Collision, spr_player, SpriteAnimation, Mouse';
+        var classes = 'Actor, Controls, Collision, spr_player, SpriteAnimation, Mouse';
         if (Game.debug) {
             classes += ', DebugCanvas, VisibleMBR';
         }
@@ -567,7 +419,7 @@ Crafty.c('Ship', {
 
 Crafty.c('Thruster', {
     init: function () {
-        this.requires("2D,Canvas,Particles")
+        this.requires("2D, Canvas, Particles")
             .attr({
                 alpha: 0.8,
                 particleDefaults: {
