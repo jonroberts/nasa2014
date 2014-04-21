3462ed0dcdf4767258fd78b0c92743307c9f4f87
mlipson <mattl920@gmail.com>
2014-04-20 22:25:04 -0400

    added same infobox style and pallate to ship purchase tooltips

--- game_v1/src/menu_items.js
+++ game_v1/src/menu_items.js
@@ -2,21 +2,25 @@ Crafty.c('BuyProbe', {
     init: function () {
         this.requires('Actor, spr_buy_probe, Mouse, HTML, Keyboard');
 
-        var info_box = Crafty.e("2D, DOM, Text, infoBox")
+//        var info_box = Crafty.e("2D, DOM, Text, infoBox")
+//            .attr({w: 75, alpha: 0.8})
+//            .text('Buy Probe ($' + Game.probe_cost.toLocaleString() + ')')
+//            .css({
+////                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
+//                'background': 'linear-gradient(#B6B4E6, #1790ED) repeat scroll 0 0 rgba(0, 0, 0, 0)',
+//                'border': '1px solid #CCCCCC',
+//                'border-radius': '15px',
+//                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
+//                'margin': '0 15px',
+//                'padding': '5px 20px',
+//                'text-shadow': '0 1px 2px #111111',
+//                'display': 'none'
+//            })
+//            .textFont({ size: '12px' });
+
+        var info_box = Crafty.e("2D, DOM, Text, infobox-"+Game.infobox_pallate)
             .attr({w: 75, alpha: 0.8})
-            .text('Buy Probe ($' + Game.probe_cost.toLocaleString() + ')')
-            .css({
-//                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'background': 'linear-gradient(#B6B4E6, #1790ED) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
+            .text('Buy Probe ($' + Game.probe_cost.toLocaleString() + ')');
 
         this.bind('Click', function (data) {
             this.createProbe();
@@ -52,21 +56,25 @@ Crafty.c('BuyShip', {
     init: function () {
         this.requires('Actor, spr_buy_ship, Mouse, HTML');
 
-        var info_box = Crafty.e("2D, DOM, Text")
+//        var info_box = Crafty.e("2D, DOM, Text")
+//            .attr({w: 75, alpha: 0.8})
+//            .text('Buy Ship ($' + Game.ship_cost.toLocaleString() + ')')
+//            .css({
+////                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
+//                'background': 'linear-gradient(#B6B4E6, #1790ED) repeat scroll 0 0 rgba(0, 0, 0, 0)',
+//                'border': '1px solid #CCCCCC',
+//                'border-radius': '15px',
+//                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
+//                'margin': '0 15px',
+//                'padding': '5px 20px',
+//                'text-shadow': '0 1px 2px #111111',
+//                'display': 'none'
+//            })
+//            .textFont({ size: '12px' });
+
+        var info_box = Crafty.e("2D, DOM, Text, infobox-"+Game.infobox_pallate)
             .attr({w: 75, alpha: 0.8})
-            .text('Buy Ship ($' + Game.ship_cost.toLocaleString() + ')')
-            .css({
-//                'background': '-moz-linear-gradient(center top , #B6B4E6 0%, #1790ED 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'background': 'linear-gradient(#B6B4E6, #1790ED) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-                'border': '1px solid #CCCCCC',
-                'border-radius': '15px',
-                'box-shadow': '0 1px 2px #FFFFFF, 0 -1px 1px #666666, 0 -1px 1px rgba(0, 0, 0, 0.5) inset, 0 1px 1px rgba(255, 255, 255, 0.8) inset',
-                'margin': '0 15px',
-                'padding': '5px 20px',
-                'text-shadow': '0 1px 2px #111111',
-                'display': 'none'
-            })
-            .textFont({ size: '12px' });
+            .text('Buy Ship ($' + Game.ship_cost.toLocaleString() + ')');
 
         this.bind('Click', function (data) {
             this.createShip();
